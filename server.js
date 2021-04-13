b26fa2a69d4c55ceac6f6d6aa8f684925b6eedc0
Amy Shackles <amyshackles@gmail.com>
2021-04-12 19:23:01 -0700

    Testing a theory

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const PORT = process.env.PORT || 5000;
 const server = express();
 server.set("subdomain offset", 0);
 server.get("/", (req, res) => {
-    console.dir(req.subdomains);
+    if (req.subdomains.includes("are")) {
+        return res.sendFile(path.join(__dirname + "/are.html"));
+    }
+    if (req.subdomains.includes("is")) {
+        return res.sendFile(path.join(__dirname + "/is.html"));
+    }
     return res.sendFile(path.join(__dirname + "/index.html"));
 });
 server.listen(PORT);
