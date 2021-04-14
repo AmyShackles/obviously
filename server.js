d1226b7e0fbddae4474ce2b85617349f6db5fd4a
Amy Shackles <amyshackles@gmail.com>
2021-04-13 22:34:58 -0700

    Adds political and disallow lists
    - Adds multiple routing paths

--- server.js
+++ server.js
@@ -1,16 +1,62 @@
 const express = require("express");
 const path = require("path");
+const { political } = require("./political.js");
+const { disallowed } = require("./disallowed.js");
+const { SentimentAnalyzer, PorterStemmer } = require("natural");
+
 const PORT = process.env.PORT || 5000;
 
 const server = express();
-server.set("subdomain offset", 0);
 server.get("/", (req, res) => {
-    if (req.subdomains.includes("are")) {
-        return res.sendFile(path.join(__dirname + "/are.html"));
+    const rootSubdomain = req.subdomains[0];
+    const subject = req.subdomains[1];
+    const analyzer = new SentimentAnalyzer("English", PorterStemmer, "afinn");
+    const sentiment = subject && analyzer.getSentiment([subject]);
+    switch (rootSubdomain) {
+        case "are":
+            if (
+                sentiment < 0 ||
+                disallowed.some((banned) => banned.includes(subject))
+            ) {
+                return res.sendFile(
+                    path.join(__dirname + "/disallowed/aredisallowed.html")
+                );
+            } else if (
+                political.some((politics) => politics.includes(subject))
+            ) {
+                return res.sendFile(path.join(__dirname + "/political/political.html"));
+            }
+            return res.sendFile(path.join(__dirname + "/is/are.html"));
+        case "is":
+            if (
+                sentiment < 0 ||
+                disallowed.some((banned) => banned.includes(subject))
+            ) {
+                return res.sendFile(
+                    path.join(__dirname + "/disallowed/isdisallowed.html")
+                );
+            } else if (
+                political.some((politics) => politics.includes(subject))
+            ) {
+                return res.sendFile(path.join(__dirname + "/political/political.html"));
+            }
+            return res.sendFile(path.join(__dirname + "/is/is.html"));
+        case "not":
+            if (req.subdomains[1] === "is") {
+                if (political.some((politics) => politics.includes(subject))
+                ) {
+                    return res.sendFile(path.join(__dirname + "/political/political.html"));
+                }
+                return res.sendFile(path.join(__dirname + "/is-not/isnot.html"));
+            } else if (req.subdomains[1] === "are") {
+                if (political.some((politics) => politics.includes(subject))
+                ) {
+                    return res.sendFile(path.join(__dirname + "/political/political.html"));
+                }
+                return res.sendFile(path.join(__dirname + "/is-not/arenot.html"));
+            }
     }
-    if (req.subdomains.includes("is")) {
-        return res.sendFile(path.join(__dirname + "/is.html"));
-    }
-    return res.sendFile(path.join(__dirname + "/index.html"));
+    return res.status(404).send("This is an unsupported use of the domain.");
 });
+
 server.listen(PORT);
